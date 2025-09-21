d33617ecb80e038ba503636a22d623a7fd3faba7
hukuitappei <btsi10.558@gmail.com>
2025-09-21 16:34:52 +0900

    first commit

---
+++ kadai.py
@@ -0,0 +1,13 @@
+#question 1,
+#問題 1 出力（基本）
+#print 関数を使って "Hello, World!" を出力してください。
+print("Hello, World!")
+#問題 2 出力（複数行）
+#print 関数を複数回使って、1、2、3 を順番に出力してください。
+print(1)
+print(2)
+print(3)
+#問題 3 出力（複数項目）
+#print 関数を使って、数値 123 と文字列 "Hello" を一行に並べて出力してください。
+
+print(1,2,3,"Hello")
