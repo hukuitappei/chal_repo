f307a42a49b9da46c08d475cb1f69ef5aeefd3cc
hukuitappei <btsi10.558@gmail.com>
2025-09-26 07:14:36 +0900

    update

--- kadai.py
+++ kadai.py
@@ -1,2 +1,3 @@
-list_2d = [[f'{x}-{y}' for y in range(10)] for x in range(10)]
-print(list_2d)
+ascii_dict = {'0x30': '0', '0x40': '@', '0x50': 'P'} 
+reversed_dict = {value: key for key, value in ascii_dict.items()}
+print(reversed_dict)
